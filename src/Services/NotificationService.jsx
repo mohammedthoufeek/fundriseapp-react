6e0ff763fe6fd378bfa04ae88c21e1284e92e240
AishwaryaE <aishwaryae28@gmail.com>
2024-03-26 19:40:36 +0530

    completed noti

--- src/Services/NotificationService.jsx
+++ src/Services/NotificationService.jsx
@@ -1,6 +1,35 @@
+
+// import { axiosInstance } from './Axios-http-client';
+
+// export const NotificationService = {
+  
+//   getAllNotificationsByUserId: async (userId) => {
+//     try {
+//       //const response = await axiosInstance.get(`http://localhost:8090/Notification/${userId}`);
+//       const response = await axiosInstance.get(`http://localhost:8090/Notification/1`);
+//       console.log(response.data)
+//       return response.data;
+//     } catch (error) {
+//       console.error('Error fetching notifications:', error);
+//       return [];
+//     }
+// }
+// };
+
+
 import { axiosInstance } from "./Axios-http-client";
 class NotificationService{
 
+  async getAllNotificationsByUserId(userId){
+    try{
+        const response=await axiosInstance.get("http://localhost:8090/Notification/1");
+        return response.data;
+    }
+    catch(error){
+        console.error("Error fetching post:",error)
+        return [];
+    }
+  }
 }
 
 export default new NotificationService();
